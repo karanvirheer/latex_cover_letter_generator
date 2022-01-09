883255d7796b7e4e0d6609231158bc4d2f9c075f
Karanvir Heer <karanvirheer@live.com>
2022-01-09 03:02:45 -0500

    Implemented working PDF creation and output.

--- generator.py
+++ generator.py
@@ -16,6 +16,30 @@ class Generator:
 
     def __init__(self, config_info: dict) -> None:
         self.config_info = config_info
-        self.variable_dict = config_info["VARIABLE_DICT"]
-        self.template_dir = config_info["TEMPLATE_DIR"]
+        self.variable_dict = config_info["VAR_TO_INPUT_DIC"]
+
+        template_dir = os.path.split(config_info["TEMPLATE_DIR"])
+        self.template_dir = Path(
+            template_dir[0] + '\\' + template_dir[1]).read_text()
+
         self.save_dir = config_info["SAVE_DIR"]
+        self.tracker_dir = config_info["TRACKER_DIR"]
+
+    def create(self):
+        print("\n============ CREATING PDF FILE ============\n")
+
+        with open('cover.tex', 'w') as f:
+            s = MyTemplate(self.template_dir)
+
+            f.write(s.substitute(self.variable_dict))
+
+        cmd = ['pdflatex', '-interaction', 'nonstopmode', 'cover.tex']
+        proc = subprocess.Popen(cmd)
+        proc.communicate()
+
+        print("\n============ DELETING FILES CREATED ============\n")
+
+        os.unlink('cover.tex')
+        os.unlink('cover.log')
+        os.unlink('cover.aux')
+        os.unlink('cover.out')
