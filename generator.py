f42b532ddfcfe8b67409d01560dd6d7c236caa40
Karanvir Heer <karanvirheer@live.com>
2022-01-12 17:44:04 -0500

    Fixed issues when settings.pickle did not exist and keys that didn't exist yet iin config_info were being accessed.

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 from pathlib import Path
 from string import Template
-import datetime
+import random
 
 
 class MyTemplate(Template):
@@ -16,16 +16,18 @@ class Generator:
 
     def __init__(self, config_info: dict) -> None:
         self.config_info = config_info
-        self.variable_dict = config_info["VAR_TO_INPUT_DIC"]
 
-        template_dir = os.path.split(config_info["TEMPLATE_DIR"])
-        self.template_dir = Path(
-            template_dir[0] + '\\' + template_dir[1]).read_text()
+        if os.path.exists("settings.pickle"):
+            self.variable_dict = config_info.get("VAR_TO_INPUT_DIC", {})
 
-        self.save_dir = config_info["SAVE_DIR"]
-        self.tracker_dir = config_info["TRACKER_DIR"]
+            template_dir = os.path.split(config_info["TEMPLATE_DIR"])
+            self.template_dir = Path(
+                template_dir[0] + '\\' + template_dir[1]).read_text()
 
-        self.excel_dataframe = pd.read_excel(self.tracker_dir)
+            self.save_dir = config_info["SAVE_DIR"]
+            self.tracker_dir = config_info["TRACKER_DIR"]
+
+            self.excel_dataframe = pd.read_excel(self.tracker_dir)
 
     def read_excel_sheet(self):
         header_dic = {}
@@ -58,3 +60,13 @@ class Generator:
         os.unlink('cover.log')
         os.unlink('cover.aux')
         os.unlink('cover.out')
+
+    def save_pdf(self):
+        output_path = f"{self.config_info['SAVE_DIR']}//{self.config_info['FILE_NAME']}.pdf"
+        if (os.path.exists(output_path)):
+            num = random.randint(0, 1000)
+            output_path = f"{self.config_info['SAVE_DIR']}//{self.config_info['FILE_NAME']} ({num}).pdf"
+
+        input_path = os.path.abspath('cover.pdf')
+        cmd = ['copy', input_path, output_path]
+        subprocess.call(cmd, shell=True)
